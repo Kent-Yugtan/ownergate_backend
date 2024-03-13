9bdfa8ab0eb029ad6c7416d5154ef01a6edb668c
k.alzoubisalinagroup1994 <k.alzoubi.itjo@salinagroup.com>
2024-03-13 06:36:54 -0700

    repair recources

--- Modules/Company/Transformers/CompanyResource.php
+++ Modules/Company/Transformers/CompanyResource.php
@@ -18,9 +18,10 @@ class CompanyResource extends JsonResource
     public function toArray($request): array
     {
         return array_merge(parent::toArray($request), [
-            'managements' => ManagementResource::collection($this->managements),
-            'news' => NewsResource::collection($this->news),
-            'services' => ServicesResource::collection($this->services),
+            'managements' => $this->managements()->paginate(10),
+            'news' => $this->news()->paginate(10),
+            'services' => $this->services()->paginate(10),
+            'locations' => $this->locations()->paginate(10),
             'attachments' => AttachmentResource::collection($this->attachments),
             'avatar' => !is_null($this->avatar) && $this->avatar !== 'null' ? route('storage.image', ['file' => $this->avatar]) : null,
             'cover_photo' => !is_null($this->cover_photo) && $this->cover_photo !== 'null' ? route('storage.image', ['file' => $this->cover_photo]) : null,
