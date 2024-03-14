1f45735517b93b964e72c187e044100c6d54c11b
Kent Yugtan <kent.yugtan95@gmail.com>
2024-03-14 20:40:34 +0800

    Feature Changes

--- Modules/Company/Transformers/CompanyResource.php
+++ Modules/Company/Transformers/CompanyResource.php
@@ -3,7 +3,9 @@
 namespace Modules\Company\Transformers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\Company\App\resources\LocationResource;
 use Modules\Company\Transformers\AttachmentResource;
+use Modules\CompanyProperty\App\resources\PropertyResource;
 
 class CompanyResource extends JsonResource
 {
@@ -17,11 +19,26 @@ class CompanyResource extends JsonResource
      */
     public function toArray($request): array
     {
+
+        $licenses = [];
+        $documents = [];
+
+        foreach ($this->attachments as $attachment) {
+            if ($attachment->type === 'license' || $attachment->type === 'License') {
+                $licenses[] = new AttachmentResource($attachment);
+            } elseif ($attachment->type === 'document' || $attachment->type === 'Document') {
+                $documents[] = new AttachmentResource($attachment);
+            }
+        }
         return array_merge(parent::toArray($request), [
             'managements' => ManagementResource::collection($this->managements),
             'news' => NewsResource::collection($this->news),
             'services' => ServicesResource::collection($this->services),
             'attachments' => AttachmentResource::collection($this->attachments),
+            'licenses' => $licenses,
+            'documents' => $documents,
+            'locations' => LocationResource::collection($this->locations),
+            'company_properties' => PropertyResource::collection($this->properties),
             'avatar' => !is_null($this->avatar) && $this->avatar !== 'null' ? route('storage.image', ['file' => $this->avatar]) : null,
             'cover_photo' => !is_null($this->cover_photo) && $this->cover_photo !== 'null' ? route('storage.image', ['file' => $this->cover_photo]) : null,
             'type' => new CompanyTypeResource($this->type)
