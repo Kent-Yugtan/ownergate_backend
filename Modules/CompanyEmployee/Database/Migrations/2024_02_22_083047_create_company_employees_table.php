77b470328dc3e4d3c11c7d58e1349eadad605866
k.alzoubisalinagroup1994 <k.alzoubi.itjo@salinagroup.com>
2024-03-22 04:34:33 -0700

    add employee id

--- Modules/CompanyEmployee/Database/Migrations/2024_02_22_083047_create_company_employees_table.php
+++ Modules/CompanyEmployee/Database/Migrations/2024_02_22_083047_create_company_employees_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
             $table->unsignedBigInteger('company_id');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('profile_id');
+            $table->string('employee_id')->nullable();
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->string('type');
             $table->string('addmail');
