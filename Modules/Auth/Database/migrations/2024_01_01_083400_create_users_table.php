0cc8fccdab839df0802089bf0246a466845e44d5
k.alzoubisalinagroup1994 <k.alzoubi.itjo@salinagroup.com>
2024-03-22 23:08:41 -0700

    update user model

--- Modules/Auth/Database/migrations/2024_01_01_083400_create_users_table.php
+++ Modules/Auth/Database/migrations/2024_01_01_083400_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->string('name')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->string('verification_token')->nullable();
             $table->timestamp('verification_date')->nullable();
