65d7d77a378030bab0370c692f9f2a4a458098ff
Karl <k.monteadora@gmail.com>
2024-06-12 16:29:38 +0800

    Fix update password

--- Modules/Auth/Http/Requests/SaveProfileRequest.php
+++ Modules/Auth/Http/Requests/SaveProfileRequest.php
@@ -2,9 +2,11 @@
 
 namespace Modules\Auth\Http\Requests;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class SaveProfileRequest extends FormRequest
 {
@@ -13,11 +15,22 @@ class SaveProfileRequest extends FormRequest
      */
     public function rules(): array
     {
+        $user = Auth::user();
+        
         return [
             'first_name' => 'required',
             'last_name' => 'required',
             'phone' => 'required|numeric',
             'password' => 'sometimes|required|string|min:8|confirmed',
+            'old_password' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) use ($user) {
+                    if (!Hash::check($value, $user->password)) {
+                        $fail('The old password is incorrect.');
+                    }
+                },
+            ],
             'avatar' => [
                 'nullable', function ($attribute, $value, $fail) {
                     if(self::hasFile($attribute)){
